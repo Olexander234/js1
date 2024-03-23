ed7d5726003ca93cdd6ebfa1d36130d67620f13f
AlexManko <mankoalex17032007@gmail.com>
2024-03-23 18:04:28 +0200

    1

---
+++ script.js
@@ -0,0 +1,83 @@
+/* let a = '3cm';
+let b = '4cm';
+let c = Math.sqrt(Math.pow(a,2))
+let a = Number.parseInt('3cm')
+let a = Number.parseInt('4cm')
+let c = Math.sqrt(Math.pow(a,2)) */
+
+/* let Message = 'Message engilsh car'
+console.log(Message.length)
+
+
+
+let riadock = 'Lets Go away';
+
+console.log(riadock.toLowerCase())
+console.log(riadock.toUpperCase())
+
+
+/* let firstName = 'car';
+console.log(firstName.index.Of( )) */
+
+
+
+let age = 14
+console.log(age)
+
+let a = 5;
+let b = 9;
+let c = a = b;
+
+console.log(c)
+
+let string = 'car'
+let mum = 150
+let boolean = true
+
+
+let myName = 'Alex'
+console.log(myName)
+
+let person = {
+	name: 'Alex',
+	age: 14,
+	isStudent: true,
+   
+};
+console.log(person)
+
+let isStudent = true
+console.log(isStudent)
+
+let myString ='Люди думають, що будуть щасливі, якщо переїдуть в інше місто, а потім виявляється: куди б ви не поїхали, ви берте із собою себе'
+console.log(myString)
+
+let myNumber = 1
+let addmyNumber = 10
+let summmyNumber = myNumber + addmyNumber
+console.log(summmyNumber)
+
+let myNull = null
+console.log(myNull)
+
+let custormName = prompt("Введіть ваше ім'я:");
+alert(`Вітаю, ${custormName}!`);
+
+
+let r = confirm("Ви впевнені, що бажаєте зробити це?");
+if (r == true) {
+  x = "Дякую за підтвердження!";
+} else {
+  x = "Дію відмінено!";
+}
+alert (x);
+
+let message = 'Дія небезпечна'
+alert(message);
+let y = confirm("Ви впевнені, що бажаєте зробити це?");
+if (b == true) {
+  x = "Дякую за підтвердження!";
+} else {
+  x = "Дію відмінено!";
+}
+alert (x);
